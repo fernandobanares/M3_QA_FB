a94807a3b9008f978f5f503d8a85b49b6d639191
Fernando <fbanarescarrasco@gmail.com>
2025-06-28 16:28:40 -0400

    creacion test

--- registros/tests.py
+++ registros/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Registro
 
-# Create your tests here.
+class RegistroTest(TestCase):
+    def test_crear_registro(self):
+        registro = Registro.objects.create(nombre="Fernando", email="fer@example.com")
+        self.assertEqual(registro.nombre, "Fernando")
+
