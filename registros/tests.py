366fa29426395f3a74e450e64c6f35b69ddb54a2
Fernando <fbanarescarrasco@gmail.com>
2025-07-13 15:08:45 -0400

    red en acceso registro eliminado

--- registros/tests.py
+++ registros/tests.py
@@ -45,6 +45,13 @@ class RegistroCRUDTest(TestCase):
         with self.assertRaises(Registro.DoesNotExist):
             Registro.objects.get(id=registro_id)
             
+    def test_acceso_registro_eliminado(self):
+        registro = Registro.objects.create(nombre="Temp", email="temp@mail.com")
+        registro_id = registro.id
+        #registro.delete()
+        with self.assertRaises(Registro.DoesNotExist):
+            Registro.objects.get(id=registro_id)
+            
     def test_email_invalido(self):
         registro = Registro(nombre="Valeria", email="noesuncorreo")
         with self.assertRaises(ValidationError):
