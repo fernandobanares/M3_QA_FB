6f9c08b439b737b9f890cb10aa7ff2b9445766f1
Fernando <fbanarescarrasco@gmail.com>
2025-07-13 14:45:56 -0400

    red en listar_registros

--- registros/tests.py
+++ registros/tests.py
@@ -6,3 +6,8 @@ class RegistroCRUDTest(TestCase):
         registro = Registro.objects.create(nombre="Fernando", email="fer@example.com")
         self.assertEqual(registro.nombre, "Fernando")
 
+    def test_listar_registros(self):
+        Registro.objects.create(nombre="Uno", email="uno@mail.com")
+        #Registro.objects.create(nombre="Dos", email="dos@mail.com")
+        registros = Registro.objects.all()
+        self.assertEqual(registros.count(), 2)
