cb5e7451c4969ed60bf73860322a3fe67cdb1445
Fernando <fbanarescarrasco@gmail.com>
2025-07-13 14:46:34 -0400

    green en listar_productos

--- registros/tests.py
+++ registros/tests.py
@@ -8,6 +8,6 @@ class RegistroCRUDTest(TestCase):
 
     def test_listar_registros(self):
         Registro.objects.create(nombre="Uno", email="uno@mail.com")
-        #Registro.objects.create(nombre="Dos", email="dos@mail.com")
+        Registro.objects.create(nombre="Dos", email="dos@mail.com")
         registros = Registro.objects.all()
         self.assertEqual(registros.count(), 2)
